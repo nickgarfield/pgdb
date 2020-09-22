b68e006a830d0f949f7e02e9cc09332c431e2328
nickgarfield <ntgarfield@gmail.com>
2020-09-22 09:38:31 -0500

    👍 IMPROVE: Cleanup parsing logic to use sqlx

--- queryItem.go
+++ queryItem.go
@@ -2,14 +2,32 @@ package sqldb
 
 import (
 	"context"
+
+	"github.com/jmoiron/sqlx"
 )
 
 // QueryItem retrieves a row from the database
 func (g *gateway) QueryItem(ctx context.Context, item interface{}, qry string, args ...interface{}) error {
+
+	// Validate 'item' is a pointer to a struct
+	_, err := isStructPtr(item)
+	if err != nil {
+		return err
+	}
+
+	// Open connection with tenant isolation
 	conn, err := g.connect(ctx)
 	if err != nil {
 		return err
 	}
+
+	// Query the rows
 	rows, err := conn.QueryContext(ctx, qry, args...)
-	return g.parseRow(rows, item)
+	if err != nil {
+		return err
+	}
+
+	// Scan row and return
+	rows.Next()
+	return sqlx.StructScan(rows, item)
 }
--- queryList.go
+++ queryList.go
@@ -2,18 +2,32 @@ package sqldb
 
 import (
 	"context"
+
+	"github.com/jmoiron/sqlx"
 )
 
 // QueryList retrieves a list of rows from the database
 func (g *gateway) QueryList(ctx context.Context, list interface{}, qry string, args ...interface{}) error {
+
+	// Validate 'list' is a pointer to a slice
+	_, err := isSlicePtr(list)
+	if err != nil {
+		return err
+	}
+
+	// Open connection with tenant isolation
 	conn, err := g.connect(ctx)
 	if err != nil {
 		return err
 	}
+
+	// Execute the query
 	rows, err := conn.QueryContext(ctx, qry, args...)
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
-	return g.parseRows(rows, list)
+
+	// Scan rows and return
+	return sqlx.StructScan(rows, list)
 }
